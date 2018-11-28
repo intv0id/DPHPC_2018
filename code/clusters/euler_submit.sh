e3efe121fdabe986ffa822fbdc9dc3056b301c28
Clément Trassoudaine <clement.trassoudaine@gmail.com>
2018-11-28 18:20:28 +0100

    Cluster job fixes

--- code/clusters/euler_submit.sh
+++ code/clusters/euler_submit.sh
@@ -7,14 +7,17 @@
 #BSUB -n 36
 #BSUB -R "span[ptile=36]"
 
+
+
+echo "Hostname=`hostname`"
+echo "Number of available cores=`nproc --all`"
+echo "Loading modules ..."
+module load new gcc/5.2.0 open_mpi/1.6.5 cmake/3.11.4 boost/1.62.0
+
 echo "Running job"
 
-hostname
-module load new gcc/5.2.0 open_mpi/1.6.5 cmake/3.11.4 boost/1.62.0
-~/PMST/code/bin/exec --algorithm ParallelSollinAL --generate-graph 100 --generate-graph 1000 --generate-graph 10000 --lsb-filename SollinParallelEL --runs 10 --max-threads 32
-
+~/PMST/code/bin/exec --algorithm ParallelSollinEL --generate-graph 100 --generate-graph 1000 --generate-graph 10000 --lsb-filename SollinParallelEL --runs 10 --max-threads 32
 ~/PMST/code/bin/exec --algorithm ParallelSollinAL --generate-graph 100 --generate-graph 1000 --generate-graph 10000 --lsb-filename SollinParallelAL --runs 10 --max-threads 32
-
 ~/PMST/code/bin/exec --algorithm Sollin --generate-graph 100 --generate-graph 1000 --generate-graph 10000 --lsb-filename Sollin --runs 10 --max-threads 1
 
 echo "Job ended"
