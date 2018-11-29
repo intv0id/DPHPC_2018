ef29da59b91a75498f11e9cddd770de6da0513fb
Clément Trassoudaine <clement.trassoudaine@gmail.com>
2018-11-29 13:37:07 +0100

    Added Kruskal algorithms test

--- code/clusters/euler_submit.sh
+++ code/clusters/euler_submit.sh
@@ -19,6 +19,8 @@ echo "Running job"
 ~/PMST/code/bin/exec --algorithm ParallelSollinEL --generate-graph 100 --generate-graph 1000 --generate-graph 10000 --lsb-filename SollinParallelEL --runs 10 --max-threads 32
 ~/PMST/code/bin/exec --algorithm ParallelSollinAL --generate-graph 100 --generate-graph 1000 --generate-graph 10000 --lsb-filename SollinParallelAL --runs 10 --max-threads 32
 ~/PMST/code/bin/exec --algorithm Sollin --generate-graph 100 --generate-graph 1000 --generate-graph 10000 --lsb-filename Sollin --runs 10 --max-threads 1
+~/PMST/code/bin/exec --algorithm FilterKruskal --generate-graph 100 --generate-graph 1000 --generate-graph 10000 --lsb-filename FilterKruskal --runs 10 --max-threads 1
+~/PMST/code/bin/exec --algorithm Kruskal --generate-graph 100 --generate-graph 1000 --generate-graph 10000 --lsb-filename Kruskal --runs 10 --max-threads 1
 
 echo "Job ended"
 
