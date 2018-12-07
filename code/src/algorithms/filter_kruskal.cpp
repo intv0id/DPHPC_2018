75659e1d672701cfbd7d77d7d9aef2e2e83b0a79
Thibault <t.dardinier@gmail.com>
2018-12-07 20:59:57 +0100

    fk

--- code/src/algorithms/filter_kruskal.cpp
+++ code/src/algorithms/filter_kruskal.cpp
@@ -13,22 +13,31 @@
 #include "pstl/execution"
 #include "pstl/algorithm"
 
-// #include <ctime>
 #include <sys/time.h>
 
 using namespace std;
 
 l_edge_t filter_kruskal::algorithm(Graph &g, unsigned int n_threads) {
+
+    struct timeval t0, t1;
+
+    gettimeofday(&t0, NULL);
+
     srand(42);
     l_edge_t result;
     vector<edge*> edges {g.unique_edges.begin(), g.unique_edges.end()};
     union_find* u_find = new union_find(g.n);
     unsigned long x = 0;
     unsigned long* old_size = &x;
+
+    gettimeofday(&t1, NULL);
+
+    cout << "Init time: " << getTime(t1, t0) << endl;
+
     return filter_kruskal_main(g, edges, u_find, old_size);
 }
 
-auto getTime(struct timeval end, struct timeval start) {
+double getTime(struct timeval end, struct timeval start) {
     return ((end.tv_sec  - start.tv_sec) * 1000000u + 
          end.tv_usec - start.tv_usec) / 1.e6;
 }
@@ -68,10 +77,7 @@ l_edge_t filter_kruskal_main(Graph &g, vector<edge*> &edges, union_find *u, unsi
         l_edge_t other_solution = filter_kruskal_main(g, e_plus, u, old_size);
         gettimeofday(&t5, NULL);
 
-        //partial_solution.splice(partial_solution.end(), other_solution);
-        // other_solution.splice(other_solution.end(), partial_solution);
-        
-        partial_solution.merge(other_solution);
+        partial_solution.splice(partial_solution.end(), other_solution);
         gettimeofday(&t6, NULL);
 
         if (old == 0) {
@@ -84,6 +90,18 @@ l_edge_t filter_kruskal_main(Graph &g, vector<edge*> &edges, union_find *u, unsi
         }
 
         return partial_solution;
+    }
+}
+
+int getMedian(vector<int> &values) {
+    tbb::parallel_sort(values.begin(), values.end());
+    int n = values.size();
+    if (n % 2 == 0) {
+        int m = n / 2;
+        return (values[m - 1] + values[m]) / 2;
+    }
+    else {
+        return values[(n - 1) / 2];
     }
 }
 
@@ -111,57 +129,21 @@ vector<edge*> filter(vector<edge*> &edges, union_find *u_find) {
 
 pair<vector<edge*>, vector<edge*>> partition(vector<edge*> &edges, int pivot) {
     
-    // edge* random_edge = edges.front();
-
     vector<edge*> e_minus = vector<edge*>(edges.size());
     vector<edge*> e_plus = vector<edge*>(edges.size());
 
-    // copy_if (
     auto it_minus = copy_if (pstl::execution::par,
-            // edges.begin(), edges.end(), back_inserter(e_minus),
             edges.begin(), edges.end(), e_minus.begin(),
             [pivot](edge* e) {return e->weight < pivot;});
 
     e_minus.resize(distance(e_minus.begin(), it_minus));
 
-    //auto it_plus = copy_if (edges.begin(), edges.end(), e_plus.begin(),
-            //[pivot](edge* e) {return e->weight > pivot;});
-
-    // copy_if (
     auto it_plus = copy_if (pstl::execution::par,
-            // edges.begin(), edges.end(), back_inserter(e_plus),
             edges.begin(), edges.end(), e_plus.begin(),
             [pivot](edge* e) {return e->weight >= pivot;});
     
     e_plus.resize(distance(e_plus.begin(), it_plus));
-    //
-    return make_pair(e_minus, e_plus);
-
-}
-
-vector<edge*> old_filter(vector<edge*> &edges, union_find *u_find) {
-    vector<edge*> filtered;
-    for (auto e : edges) {
-        if (u_find->find(e->source) != u_find->find(e->target)) {
-            filtered.push_back(e);
-        }
-    }
-    return filtered;
-}
-
-pair<vector<edge*>, vector<edge*>> old_partition(vector<edge*> &edges, int pivot) {
-
-    vector<edge*> e_minus;
-    vector<edge*> e_plus;
-
-    for (auto e : edges){
-        if (e->weight <= pivot) {
-            e_minus.push_back(e);
-        }
-        else {
-            e_plus.push_back(e);
-        }
-    }
 
     return make_pair(e_minus, e_plus);
+
 }
