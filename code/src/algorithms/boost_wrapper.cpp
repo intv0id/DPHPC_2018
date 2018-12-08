b75d27507a85304ab3318f053f77f872a75cf065
Thibault <t.dardinier@gmail.com>
2018-12-08 11:53:26 +0100

    Boost

--- code/src/algorithms/boost_wrapper.cpp
+++ code/src/algorithms/boost_wrapper.cpp
@@ -2,10 +2,14 @@
 #include <algorithm>
 #include <map>
 #include <cstdlib>
+#include <iostream>
 
 #include "algorithms/boost_wrapper.hpp"
 #include <boost/graph/kruskal_min_spanning_tree.hpp>
 // #include <boost/graph/prim_minimum_spanning_tree.hpp>
+
+#include <sys/time.h>
+#include "algorithms/filter_kruskal.hpp"
 
 using namespace std;
 
@@ -14,7 +18,13 @@ l_edge_t boost_wrapper::algorithm(Graph &g, unsigned int n_threads) {
     boost::property_map<Boost_Graph,boost::edge_weight_t>::type w = get(boost::edge_weight, g.boost_rep);
     vector<Boost_Edge> v;
 
+    struct timeval t0, t1;
+    gettimeofday(&t0, NULL);
+
 	kruskal_minimum_spanning_tree(g.boost_rep, back_inserter(v));
+    gettimeofday(&t1, NULL);
+
+    std::cout << "BOOST: " << getTime(t1, t0) << std::endl;
 
 	l_edge_t result;
 
