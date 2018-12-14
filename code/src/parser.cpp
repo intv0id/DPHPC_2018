92533b5c244f1d1077e87fdf8d59676e397824be
Thibault <t.dardinier@gmail.com>
2018-12-14 13:45:14 +0100

    .bak ed prim

--- code/src/parser.cpp
+++ code/src/parser.cpp
@@ -10,7 +10,7 @@
 #include "algorithms/sollin.hpp"
 #include "algorithms/kruskal.hpp"
 #include "algorithms/filter_kruskal.hpp"
-#include "algorithms/prim.hpp"
+// #include "algorithms/prim.hpp"
 #include "algorithms/boost_wrapper.hpp"
 #include "algorithms/filter_sollin.hpp"
 
