101e8f1dd232fc952b78ff5c0d3011d8760f69e6
Thibault <t.dardinier@gmail.com>
2019-01-11 15:26:32 +0100

    Special data

---
+++ data/lsb/FAL/FAL_USA-NW/lsb.ParallelSollinFAL_timing_USA_NW.r2
@@ -0,0 +1,13 @@
+# Sysname : Linux
+# Nodename: eu-a6-007-04
+# Release : 3.10.0-862.14.4.el7.x86_64
+# Version : #1 SMP Wed Sep 26 15:12:11 UTC 2018
+# Machine : x86_64
+# Execution time and date (UTC): Fri Jan 11 02:50:40 2019
+# Execution time and date (local): Fri Jan 11 03:50:40 2019
+# MPI execution on rank 0 with 1 processes in world
+# Reported time measurements are in microseconds
+# pretty output format
+    Algorithm  Graph_nodes   Graph_name  Max_threads          run       id         time     overhead 
+ parallel_sollin_FAL       1207945   USA-road-t.             1             0          0 11399751.239746            0 
+# Runtime: 19.445808 s (overhead: 0.000000 %) 1 records
---
+++ data/plot.py
@@ -0,0 +1,57 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+for f1 in os.listdir("lsb"):
+    for f2 in os.listdir("lsb/" + f1):
+
+        folder = "lsb/" + f1 + "/" + f2
+        files = os.listdir(folder)
+
+        algo = f1
+        graph = f2.split("_")[1]
+
+        data = {}
+        the_x = []
+        the_y = []
+
+        for name in files:
+            print(name)
+            f = open(folder + "/" + name, "r")
+            l = f.readlines()
+            if len(l) > 0:
+                x = l[11].split(" ")
+                n = int("".join(x[20:30]))
+                y = float("".join(x[45:55]))
+                if n not in data:
+                    data[n] = []
+                data[n].append(y)
+                the_x.append(n)
+                the_y.append(y)
+
+        if len(the_x) > 0:
+
+            base = np.median(data[1])
+            unique_x = sorted(data.keys())
+            unique_y = [np.median(data[x]) for x in unique_x]
+            avg_y = [np.average(data[x]) for x in unique_x]
+
+            speedup = [base / y for y in the_y]
+            unique_speedup = [base / y for y in unique_y]
+            avg_speedup = [base / y for y in avg_y]
+
+            plt.figure(figsize=(10, 8))
+            plt.scatter(the_x, speedup)
+            plt.plot(unique_x, unique_speedup, label="Median")
+            plt.plot(unique_x, avg_speedup, label="Average")
+            plt.title("Speedup " + algo + " " + graph)
+            plt.legend()
+            plt.savefig("images/speedup_" + algo + "_" + graph + ".png")
+
+            plt.figure(figsize=(10, 8))
+            plt.scatter(the_x, the_y)
+            plt.plot(unique_x, unique_y, label="Median")
+            plt.plot(unique_x, avg_y, label="Average")
+            plt.title("Time " + algo + " " + graph)
+            plt.legend()
+            plt.savefig("images/time_" + algo + "_" + graph + ".png")
