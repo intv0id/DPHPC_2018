8e869fa2ab3f0c79d7afa801d8cbeaa8e404d3dc
Thibault <t.dardinier@gmail.com>
2019-01-13 18:55:44 +0100

    Data

--- code/submit.sh
+++ code/submit.sh
@@ -7,8 +7,6 @@
 #BSUB -n 36
 #BSUB -R "span[ptile=36]"
 
-
-
 module load new gcc/5.2.0 open_mpi/1.6.5 cmake/3.11.4 boost/1.62.0 ;
 
 PMST_PATH="`readlink -f ~`/PMST/code" ; 
---
+++ data/clone/dispatch.sh
@@ -0,0 +1,18 @@
+#! /bin/bash -l
+
+types=("ER" "PA");
+declare -A algos=( [FAL]=FAL [AL]=AL [FS]=FilterSollin );
+numbers=(50 10);
+
+for short_algo in "${!algos[@]}"
+do
+    algo=${algos[$short_algo]}
+    echo "$algo"
+    for type in "${types[@]}"
+    do
+        for number in "${numbers[@]}"
+	do
+	    mv lsb.*$algo*$type*$number* ../lsb/$short_algo/$short_algo\_$type-$number
+        done
+    done
+done
