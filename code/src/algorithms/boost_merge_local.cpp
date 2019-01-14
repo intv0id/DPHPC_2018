17d0b472a2ba12b6fb81ab927b6f1287ce7efc14
cambiert <cambiert@student.ethz.ch>
2019-01-14 12:24:35 +0100

    added missing files

---
+++ code/src/algorithms/boost_merge_local.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <algorithm>
+#include <map>
+#include <cstdlib>
+#include <iostream>
+
+#include "algorithms/boost_merge_local.hpp"
+#include <boost/graph/distributed/dehne_gotz_min_spanning_tree.hpp>
+#include <boost/graph/distributed/vertex_list_adaptor.hpp>
+
+#include <sys/time.h>
+
+using namespace std;
+
+l_edge_t boost_merge_local::algorithm(Graph &g, unsigned int n_threads) {
+
+    boost::property_map<Boost_DistribGraph, boost::edge_weight_t>::type w = get(boost::edge_weight, g.boost_distrib_rep);
+    
+    vector<DistriBoost_Edge> v;
+   
+    struct timeval t0, t1;
+    gettimeofday(&t0, NULL);
+ 
+    merge_local_minimum_spanning_trees(make_vertex_list_adaptor(g.boost_distrib_rep), w, back_inserter(v),  get(boost::vertex_index, g.boost_distrib_rep));
+    gettimeofday(&t1, NULL);
+
+    
+    l_edge_t result;
+    
+
+    for (vector<DistriBoost_Edge>::iterator it = v.begin(); it != v.end(); it++) {
+       if (process_id(g.boost_distrib_rep.process_group()) == 0) {
+          edge* e = new edge();
+          DistriBoost_Vertex u1 = boost::source(*it, g.boost_distrib_rep);
+          DistriBoost_Vertex u2 = boost::target(*it, g.boost_distrib_rep);
+	  
+	  e->source=g.boost_distrib_rep.distribution().global(owner(u1), local(u1));
+    	  e->target=g.boost_distrib_rep.distribution().global(owner(u2), local(u2));
+	  e->weight=w[*it];
+          result.push_back(e);
+       }
+   }
+
+    return result;
+}
